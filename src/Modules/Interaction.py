3a25c22fc819e6220bd1aa67d9c6221b9696550f
Juan Trejos <juan.trejos@arandasoft.com>
2023-09-09 15:34:23 -0500

    Add history

--- src/Modules/Interaction.py
+++ src/Modules/Interaction.py
@@ -1,5 +1,7 @@
 import telebot
 import time
+import json
+import datetime
 
 from src.configuration import NUMBERS
 
@@ -14,14 +16,33 @@ class Interaction:
     def comprobe_number(bot, message):
         phone_number = message.contact.phone_number
         number = phone_number[-10:]
-        mensaje = "url (Se eliminará en 30s)"
         chat_id = message.chat.id
+        contact = message.contact
 
-        message = bot.send_message(chat_id, mensaje)
-        message_id = message.message_id
+        fecha_actual = datetime.datetime.now()
 
-        if number in NUMBERS:
+        user_list = []
+
+        with open('src/data/history.json', "r") as archivo:
+            user_list = json.load(archivo)
+
+        user_data = {
+            'user_id': message.from_user.id,
+            'first_name': message.from_user.first_name,
+            'last_name': message.from_user.last_name,
+            'phone_number': contact.phone_number,
+            'date': fecha_actual.strftime("%d/%m/%Y %H:%M")
+        }
+
+        user_list.append(user_data)
+
+        with open('src/data/history.json', 'w') as file:
+            json.dump(user_list, file, indent=4)
+
+        if (number in NUMBERS):
+            message = bot.send_message(chat_id, "url (Se eliminará en 30s)")
+            message_id = message.message_id
             time.sleep(60)
             bot.delete_message(chat_id, message_id)
         else:
-            bot.send_message(message.from_user.id, f"No")
+            bot.send_message(chat_id, "No tiene permiso")
